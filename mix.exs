4e90d0fc34a7658bd96423229c20c859e6b52844
xvw <xavier.vdw@gmail.com>
2017-05-08 04:16:30 +0200

    Complete Mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,27 @@
 defmodule Kronos.Ecto.Mixfile do
   use Mix.Project
 
+
+  defp description do 
+    """
+    Provide Custom Types for Kronos.t (simple timestamps).
+    """
+  end
+
   def project do
-    [app: :kronos_ecto,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :kronos_ecto,
+      version: "0.1.0",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      source_url: "https://github.com/xvw/kronos_ecto",
+      homepage_url: "https://github.com/xvw/kronos_ecto",
+      deps: deps(),
+      package: package(),
+      description: description(),
+      docs: docs()
+    ]
   end
 
   # Configuration for the OTP application
@@ -30,7 +44,27 @@ defmodule Kronos.Ecto.Mixfile do
   defp deps do
     [
       {:kronos, "~> 1.0.0"},
-      {:ecto, "~> 2.1.0"}
+      {:ecto, "~> 2.1.0"},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
     ]
+  end
+
+  def docs do 
+    [
+      main: "readme",
+      extras: [
+        "README.md"
+      ]
+    ]
+  end
+
+  defp package do
+    [
+     name: :kronos_ecto,
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Xavier Van de Woestyne"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/xvw/kronos_ecto",
+              "Docs" => "http://xvw.github.io/kronos_ecto/doc/readme.html"}]
   end
 end
